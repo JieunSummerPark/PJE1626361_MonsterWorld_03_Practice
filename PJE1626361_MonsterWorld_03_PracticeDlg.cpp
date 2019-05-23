3a300e195b01f9ec4bf94a77699f3b3c06ac1599
JieunSummerPark <parkj2@student.swosu.edu>
2019-05-23 10:59:40 +0900

    기본

--- PJE1626361_MonsterWorld_03_PracticeDlg.cpp
+++ PJE1626361_MonsterWorld_03_PracticeDlg.cpp
@@ -8,6 +8,10 @@
 #include "PJE1626361_MonsterWorld_03_PracticeDlg.h"
 #include "afxdialogex.h"
 
+#include "MonsterWorld.h"
+#include "VariousMonsters.h"
+#include <time.h>
+
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
@@ -158,5 +162,15 @@ HCURSOR CPJE1626361MonsterWorld03PracticeDlg::OnQueryDragIcon()
 
 void CPJE1626361MonsterWorld03PracticeDlg::OnBnClickedButton1()
 {
-	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	srand((unsigned int)time(NULL));
+	int w = 16, h = 8;
+	MonsterWorld game(w, h);
+
+	game.add(new Zombie("허접한좀비", "◎", rand() % w, rand() % h));
+	game.add(new Vampire("뱀파이어짱", "★", rand() % w, rand() % h));
+	game.add(new KGhost("어쩌다귀신", "♥", rand() % w, rand() % h));
+	game.add(new Jiangshi("못먹어도고", "↔", rand() % w, rand() % h, true));
+	game.add(new Jiangshi("못먹어세로", "↕", rand() % w, rand() % h, false));
+	game.play(500, 10);
+	printf("------게임 종료-------------------\n");
 }
